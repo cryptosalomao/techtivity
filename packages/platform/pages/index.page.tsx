74f06a8b788e1c5190227867808290d0afa3affd
Guilherme Salomão <cryptosalomao@gmail.com>
2023-05-19 18:33:27 -0300

    chore(router): redirect root route to /projects [#1]

--- packages/platform/pages/index.page.tsx
+++ packages/platform/pages/index.page.tsx
@@ -1,4 +1,13 @@
 import Head from "next/head"
+import { route, type GetServerSidePropsContext } from "nextjs-routes"
+
+export function getServerSideProps(context: GetServerSidePropsContext) {
+  return {
+    redirect: {
+      destination: route({ pathname: "/projects" }),
+    },
+  }
+}
 
 export default function Page() {
   return (
